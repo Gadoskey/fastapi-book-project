d7b0b83c1e9b9b050acb40e01e03aa9ec6c2a174
Yusuf Mustapha Opeyemi <138693302+Gadoskey@users.noreply.github.com>
2025-02-13 21:37:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,3 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
-
-@app.get("/mytest")
-async def mytest():
-    return {"my test deployed and changed"}
